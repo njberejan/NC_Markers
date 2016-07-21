404ae6eaa305fbf71dda96e532ad98a5fe0d7267
Nick Berejan <njberejan@gmail.com>
2016-07-21 15:32:24 -0400

    updated settings

--- NC_markers/NC_markers/settings.py
+++ NC_markers/NC_markers/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'marker_app'
+    'marker_app.apps.MarkerAppConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE_CLASSES = [
