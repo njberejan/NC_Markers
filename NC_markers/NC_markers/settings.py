7cc1b08f3171536e660f8ced692d3836b42c24c9
Nick Berejan <njberejan@gmail.com>
2016-07-21 15:10:42 -0400

    model migrated

--- NC_markers/NC_markers/settings.py
+++ NC_markers/NC_markers/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,17 +75,6 @@ WSGI_APPLICATION = 'NC_markers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nc_markers',
-        'USER': 'Nick',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
 
 
 # Password validation
